b8467a3ebfeb43129149bcfe28fe64553bbeac7f
sarlinvijay <sarlinvijay12@gmail.com>
2022-03-10 22:59:27 +0530

    new code

--- src/test/java/org/test/SamplePage.java
+++ src/test/java/org/test/SamplePage.java
@@ -1,5 +1,25 @@
 package org.test;
 
-public class SamplePage {
+import org.testng.annotations.Test;
 
+public class SamplePage {
+	
+	
+	
+	
+	@Test
+	private void test() {
+		// TODO Auto-generated method stub
+
+		
+	
+	
+	}
+
+	
+	@Test
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
 }
